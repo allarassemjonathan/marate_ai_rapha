e03775586c33cae42571bf355a96a616185477c3
Jonathan Allarassem <jonat@5CG024DGD4.gcc.edu>
2025-07-22 00:53:10 -0400

    last time ...

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ import io
 import os
 import signal
 import sys
+import json
 
 load_dotenv()
 DATABASE = 'patients.db'
@@ -32,6 +33,22 @@ SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
 def authenticate():
     """Authenticate and return Drive service"""
+
+    info_cred = {
+        "installed":{
+                "client_id": os.environ.get("client_id"),
+                "project_id": os.environ.get("project_id"),
+                "auth_uri": os.environ.get("auth_uri"), 
+                "token_uri":os.environ.get("token_uri"),
+                "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+                "client_secret": os.environ.get("client_secret"),
+                "redirect_uris":[os.environ.get("redirect_uris")]
+        }
+    }
+
+    with open("credentials.json", "w", encoding="utf-8") as f:
+        json.dump(info_cred, f, indent=4)
+        
     creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
