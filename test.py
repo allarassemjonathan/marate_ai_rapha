229b3ad2b127d405f6812e47e0ba64fe4eb25d82
Jonathan Allarassem <jonat@5CG024DGD4.gcc.edu>
2025-08-21 22:58:26 -0400

    size is fixed table

--- test.py
+++ test.py
@@ -10,11 +10,12 @@ conn = psycopg2.connect("postgresql://postgres:QUFfTNqAGwNVhBWGbIkkxKfxEyHogGKE@
 cur = conn.cursor()
 cur.execute("SELECT * FROM patients")
 rows = cur.fetchall()
+
 columns = [desc[0] for desc in cur.description]
 df = pd.DataFrame(rows, columns=columns)
 
 # Count occurrences of each 'adresse'
-adresse_counts = df['adresse'].value_counts()
+adresse_counts = df['adresse'].value_counts()[0:10]
 
 # Plot
 plt.figure(figsize=(10, 6))
@@ -25,5 +26,3 @@ plt.ylabel("Nombre de patients")
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
-
-print('Melo la star has modified something in the code')
